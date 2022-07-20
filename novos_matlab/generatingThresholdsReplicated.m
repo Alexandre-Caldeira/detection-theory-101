1e2e67485cb9713049658143313e1126b72c155e
Alexandre-Caldeira <alexandre.caldeira@ufv.br>
2022-07-20 13:03:06 -0300

    investigando ht2 iniciado

--- novos_matlab/generatingThresholdsReplicated.m
+++ novos_matlab/generatingThresholdsReplicated.m
@@ -1,7 +1,8 @@
-K               = 5;                            % analyse data 3 times
-TotalAlpha      = 0.05;                         % pre-specified alpha-level for the full sequential test
-alphaVector     = ones(1,K)*(TotalAlpha/K);     % alpha to spend at each stage
+% clearvars;clc;close all
+K               = 5;                            % analyse data K times
+TotalAlpha      = 0.01;                         % alpha for the full test
+alphaVector     = ones(1,K)*(TotalAlpha/K); 
 gammaVector     = [0.1, 0.15, 0.2, 0.25, 0.29];
-Thresholds      = chesnayeThresholds(K,alphaVector);
+Thresholds      = chesnayeThresholds(K,alphaVector,gammaVector);
 disp(Thresholds)
 save('5Thresholds.mat','Thresholds')
